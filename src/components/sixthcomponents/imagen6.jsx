5f5afb5ff118d94fd746db767ca2a8ee0152ee2f
Diego.ruiz <dianruro8223@otmail.com>
2024-09-25 17:52:09 -0500

    commit6

---
+++ src/components/sixthcomponents/imagen6.jsx
@@ -0,0 +1,12 @@
+import imagen6 from ' ../assets/img/imagen6.jpg'
+import PropTypes: from 'prop-types';
+
+export const imagen6 = ({this.props.}) => {
+  return (
+     <img src={imagen6} alt="paisajes del mundo" 
+     className={props.className}/>
+  )
+}
+imagen6.proptotype = {
+    className= PropTypes.string
+}
