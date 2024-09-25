3ec347bb4b53734777dcf27b10573a784b81bfb5
Diego.ruiz <dianruro8223@otmail.com>
2024-09-25 16:42:03 -0500

    commit3

---
+++ src/components/imagen1.jsx
@@ -0,0 +1,13 @@
+import imagen1 from ' ../assets/img/imagen1.jpg'
+import PropTypes: from 'prop-types';
+
+export const imagen1 = ({this.props.}) => {
+  return (
+     <img src={imagen1} alt="paisajes del mundo" 
+     className={props.className}/>
+  )
+}
+imagen1.proptotype = {
+    className= PropTypes.string
+}
+
